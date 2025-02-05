3e2fbfe10f3e4ab4efeaa10e54b93c5a75510d88
Quang <126427971+theantigone@users.noreply.github.com>
2025-02-05 01:09:53 -0500

    updated ayush's files

--- converter_file.py
+++ converter_file.py
@@ -1,7 +1,7 @@
 from music21 import converter
 
-score = converter.parse("C:/Users/Ayush/OneDrive/Documents/Audiveris/Double Note Scale/Double Note Scale.mxl")
+score = converter.parse("./testcases/Mizore and Ririka Oboe Duet.mxl")
 
-score.write("midi", fp="C:/Users/Ayush/OneDrive/Documents/Audiveris/Double Note Scale/Double Note Scale.midi")
+score.write("midi", fp="./testcases/mxl_to_midi/Mizore and Ririka Oboe Duet.midi")
 
 print("MIDI file created.")
