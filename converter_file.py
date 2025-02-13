fa87cb00b81cf929ac90eee06674d56df0576b50
Kyle Chau <kylechau@Kyles-Air.fios-router.home>
2025-02-13 01:42:54 -0500

    cleaned up main branch

--- converter_file.py
+++
@@ -1,7 +0,0 @@
-    from music21 import converter
-
-score = converter.parse("./testcases/Mizore and Ririka Oboe Duet.mxl")
-
-score.write("midi", fp="./testcases/mxl_to_midi/Mizore and Ririka Oboe Duet.midi")
-
-print("MIDI file created.")
